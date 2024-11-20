2d6db55318055044181bd266d8893015419fc244
Karolina Wysocka <karolinawyso@mat.umk.pl>
2024-11-20 17:05:55 +0100

    Zmiana w repo1 na g3

--- p1.c
+++ p1.c
@@ -1,13 +1,6 @@
 #include <stdio.h>
 
 int main(){
-	printf("mod w repo2");
-	printf("mod w repoLB");
 	printf("Hello World!");
-	printf("Elo elo 320");
-	printf("Pozdr");
-	printf("hello");
-	printf("Modyfikacja");
-	printf("nwm");
 	return 0;
  }
