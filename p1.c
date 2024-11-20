d3b892ac090ad95f46e80a322e38372247686405
Karolina Wysocka <karolinawyso@mat.umk.pl>
2024-11-20 16:50:06 +0100

    Zmiana w p1.c

--- p1.c
+++ p1.c
@@ -1,11 +1,8 @@
 #include <stdio.h>
 
 int main(){
-<<<<<<< HEAD
 	printf("mod w repo2");
-=======
 	printf("mod w repoLB");
->>>>>>> 74956738401dadcf929ea5917afbfed330e08a88
 	printf("Hello World!");
 	printf("Elo elo 320");
 	printf("Pozdr");
